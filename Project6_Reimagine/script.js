af83a83e806424fcde866d481da736b490a64718
Zoe Anderson <zande@unc.edu>
2024-12-08 20:59:14 -0500

    push update

--- Project6_Reimagine/script.js
+++ Project6_Reimagine/script.js
@@ -10,12 +10,17 @@
 
 let isMember = false;
 let username = "";
+let pfpSrc = "";
 let karma = 0;
 let numPosts = 0;
+const DEFAULT_COLORS = ["#FFFFFF", "#000000", "#CCCCCC", "#333333"];
+let style = {
+    colors: DEFAULT_COLORS, 
+}
 //each post has an id, title, number of upvotes, array of comments, and bodyText
 
 class Post {
-    constructor(user, title, bodyText) {
+    constructor(user, title, bodyText, color) {
         this.id = numPosts;
         numPosts++;
         this.user = user;
@@ -23,6 +28,7 @@ class Post {
         this.bodyText = bodyText;
         this.numUpvotes = 0;
         this.comments = [];
+        this.color = color;
         this.div = this.toDiv();
     }
 
@@ -34,6 +40,7 @@ class Post {
         }
 
         const rv = quickCreate('section', 'post');
+        rv.style.backgroundColor = style.colors[this.color];
 
         const title = quickCreate('div', 'post-title');
         title.innerText = this.title;
@@ -90,6 +97,7 @@ class Post {
 
     upvote(){
         this.numUpvotes++;
+        karma++;
         const counter = this.div.querySelector('.upvote-count');
         counter.innerText = this.numUpvotes;
     }
@@ -102,8 +110,9 @@ class Post {
 }
 
 let posts = [
-    new Post("Zoe", "My first post", ""),
-    new Post("Kelsey", "my second post", "")
+    new Post("Zoe", "Don't you guys think our subreddit is super boring looking?", 
+        "I mean what's even the point of visiting a subreddit that looks so dull and uninteresting?", "0"),
+    new Post("Kelsey", "my second post", "", "0")
 ];
 
 class Comment {
@@ -111,16 +120,27 @@ class Comment {
         this.user = user;
         this.commentText = commentText;
     }
+
+    toDiv(){
+
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     //Load page
-    const main = document.querySelector('main');
-    for (let post of posts){
-        if (post != null){
-            main.appendChild(post.div);
+    render();
+
+    //Event Listeners
+    //data-action global event listener strategy provided by ChatGPT
+    //basically checks every click to see if its on an element that is tagged as having the following behavior
+    document.addEventListener('click', function (event) {
+        if (event.target.matches('[data-action="close"]')) {
+            const parent = event.target.closest('.target-parent');
+            if (parent) {
+                parent.style.display = 'none';
+            }
         }
-    }
+    });
 
     //User setup
     if(!localStorage.getItem('username')){
@@ -134,9 +154,26 @@ document.addEventListener('DOMContentLoaded', function() {
                 username = userInput.value;
                 welcome.style.display = 'none';
                 document.body.style.overflow = "visible";
+                document.getElementById('username').innerText = username;
             }
         });
     }
+    else{
+        username = localStorage.getItem('username');
+    }
+    if(localStorage.getItem('pfpSrc')){
+        pfpSrc = localStorage.getItem('pfpSrc');
+    }
+    if(localStorage.getItem('karma')){
+        karma = localStorage.getItem('karma');
+    }
+    if(localStorage.getItem('isMember')){
+        isMember = localStorage.getItem('isMember');
+    }
+    if(localStorage.getItem('style')){
+        style = localStorage.getItem('style');
+    }
+
 });
 
 function joinLeaveSubreddit(){
@@ -150,37 +187,82 @@ function joinLeaveSubreddit(){
     }
 }
 
+function showEditDialog(){
+    const edit = document.getElementById('edit');
+    edit.style.display = 'block';
+}
+
+function showMakePost(){
+    const makePost = document.getElementById('make-post');
+    colors = makePost.querySelectorAll('.color');
+    colors.forEach((color, i) => {
+        color.style.backgroundColor = style.colors[i];
+      });
+    makePost.style.display = 'block';
+}
+
+function render(){
+    const main = document.querySelector('main');
+    for (let post of posts){
+        if (post != null){
+            main.appendChild(post.div);
+        }
+    }
+}
+
+function makePost(){
+    const title = document.getElementById('new-post-title').value;
+    const text = document.getElementById('new-post-text').value;
+    let color;
+    const radios = document.querySelectorAll('input[name="color"]');
+    radios.forEach(radio => {
+        if (radio.checked) {
+            color = radio;
+        }
+    });
+
+    const post = new Post(username, title, text, color);
+    posts.push(post);
+    render();
+
+}
+
+function writeComment(){
+
+}
+
+function postComment(){
+
+}
+
+function fileUpload(triggerElementId, parentDivId){
+    const parent = document.getElementById(parentDivId);
+    const input = document.getElementById(triggerElementId).files[0];
+    //FileReader explaination provided by ChatGPT
+    if (input) {
+        const reader = new FileReader(); // Create a FileReader
+
+        // When the file is read, display it as an image
+        reader.onload = (e) => {
+          parent.src = e.target.result; // Set the image source
+          parent.alt = 'Uploaded Image';
+        };
+
+        reader.readAsDataURL(input); // Read the file as a data URL
+      }
+      console.log("complete!");
+}
+
+function applyEdit(){
+    
+}
+
 function report(){
 
 }
 
 function checkAccessibility(){
 
-}
-
-//fill divs with icons
-const comments = document.getElementsByClassName('comment');
-const dots = document.getElementsByClassName('dot');
-const downvotes = document.getElementsByClassName('downvote');
-const upvotes = document.getElementsByClassName('upvote');
-
-for(let comment of comments){
-    comment.innerHTML += '<img src="./Graphics/comment.svg">';
-}
-for(let dot of dots){
-    dot.innerHTML += `<img src="./Graphics/dots.svg">`;
-}
-let vote;
-for(let upvote of upvotes){
-    vote = document.createElement('img');
-    vote.setAttribute('src', './Graphics/arrow-up.png');
-    upvote.appendChild(vote);
-}
-for(let downvote of downvotes){
-    vote = document.createElement('img');
-    vote.setAttribute('src', './Graphics/arrow-up.png');
-    vote.style.transform = "rotate(180deg)";
-    downvote.appendChild(vote);
 }
 
 //get user data
